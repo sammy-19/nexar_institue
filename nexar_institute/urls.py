4b4e989f1fd13e049ab7573afe60638998c88491
Samson Phiri <sammyhills@gmail.com>
2025-05-05 08:45:52 +0300

    initial commit for vercel configuration

--- nexar_institute/urls.py
+++ nexar_institute/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -33,3 +32,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
