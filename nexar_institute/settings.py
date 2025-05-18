b292321935b0e055480be8012f8ff80e7f3ff06e
Samson Phiri <sammyhills@gmail.com>
2025-05-18 10:17:17 +0300

    added pymysql

--- nexar_institute/settings.py
+++ nexar_institute/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@t(8lihjd2taq2cfkx5cjdzhazvl#en5-!1-@pge#(ptt+zta$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 # Application definition
 INSTALLED_APPS = [
